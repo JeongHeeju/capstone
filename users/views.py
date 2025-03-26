f93d3f45b8c35762f3ae85d1487862bb5b44c18d
JeongHeeju <134275949+JeongHeeju@users.noreply.github.com>
2025-03-26 13:22:49 +0900

    Update views.py

--- users/views.py
+++ users/views.py
@@ -3,13 +3,16 @@ from django.contrib.auth import login, authenticate
 from .forms import UserRegisterForm, UserLoginForm
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
-from .models import User, UserProfile
-from .serializers import UserSerializer, UserLoginSerializer, UserProfileSerializer, FoodPreferenceSerializer, AllergySerializer, UserProfileSerializer,PersonalInfoSerializer
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from .models import User, UserProfile, UserSurveyRecord
+from .serializers import (
+    UserSerializer, UserLoginSerializer, UserProfileSerializer,
+    FoodPreferenceSerializer, AllergySerializer, PersonalInfoSerializer
+)
 from django.contrib.auth.hashers import make_password
 from django.http import JsonResponse
-from rest_framework.permissions import IsAuthenticated, AllowAny
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework.authtoken.models import Token
 
 
 @api_view(['POST'])
@@ -27,63 +30,67 @@ def signup(request):
     except Exception as e:
         return Response({'error': str(e)}, status=400)
 
+
 @api_view(['POST'])
 def login_view(request):
     data = request.data
     user = authenticate(user_id=data['user_id'], password=data['password'])
+    print("DEBUG: user =", user, "| type =", type(user))
     if user:
-        return Response({'message': 'Login successful', 'user_id': user.user_id}, status=200)
+      token, _ = Token.objects.get_or_create(user=user)
+      return Response({'message': 'Login successful', 'user_id': user.user_id, 'token': token.key}, status=200)
     return Response({'error': 'Invalid credentials'}, status=400)
+    print('로그인된 user:',user,type(user))
 
 
-#설문조사 저장 부분
+# 설문조사 저장 부분
 @api_view(['POST'])
-@permission_classes([AllowAny])
+@permission_classes([IsAuthenticated])
 def save_personal_info(request):
     data = request.data  
-    serializer = UserProfileSerializer(data=data)  
-    test_user = User.objects.get(user_id='qqqq1111')
-    user_profile = UserProfile.objects.filter(user_id=test_user).first()
+    user = request.user
+    user_profile = UserProfile.objects.filter(user_id=user).first()
     if not user_profile:
-      user_profile = UserProfile(user_id=test_user)
-    
+        user_profile = UserProfile(user_id=user)
+
     serializer = PersonalInfoSerializer(instance=user_profile, data=data, partial=True)
     if serializer.is_valid():
-        serializer.save(user_id=test_user) 
+        serializer.save(user_id=user)
         return Response({'message': 'Personal information saved successfully'}, status=status.HTTP_201_CREATED)
     else:
-        print("Validation failed:", serializer.errors)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)  
-
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['POST'])
+@permission_classes([IsAuthenticated])
 def save_food_preferences(request):
     data = request.data
-    user_profile = UserProfile.objects.get(user_id__user_id='qqqq1111')  
+    user_profile = UserProfile.objects.get(user_id=request.user)
     for item in data:
         serializer = FoodPreferenceSerializer(data=item)
         if serializer.is_valid():
-            serializer.save(user=user_profile)  
+            serializer.save(user=user_profile)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     return Response({'message': 'Food preferences saved successfully'}, status=status.HTTP_201_CREATED)
 
 
 @api_view(['POST'])
+@permission_classes([IsAuthenticated])
 def save_allergy_info(request):
     data = request.data
-    user_profile = UserProfile.objects.get(user_id__user_id='qqqq1111')  # 사용자 프로필 가져오기
+    user_profile = UserProfile.objects.get(user_id=request.user)  # 사용자 프로필 가져오기
     for item in data:
         serializer = AllergySerializer(data=item)
         if serializer.is_valid():
-            serializer.save(user=user_profile)  # 사용자 프로필 연결
+            serializer.save(user=user_profile)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     return Response({'message': 'Allergy information saved successfully'}, status=status.HTTP_201_CREATED)
 
+
 @api_view(['POST'])
-@permission_classes([AllowAny])  # 인증을 무시해서 테스트 쉽게
+@permission_classes([IsAuthenticated])
 def save_all_in_one(request):
     serializer = UserProfileSerializer(data=request.data)
     if serializer.is_valid():
