1f75138406da0041961c0d9e3d60e88af9272139
JeongHeeju <134275949+JeongHeeju@users.noreply.github.com>
2025-03-26 13:24:21 +0900

    Update models.py

--- users/models.py
+++ users/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, User
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, User , PermissionsMixin
 
 class UserManager(BaseUserManager):
     def create_user(self, user_id, username, password=None):
@@ -13,23 +13,28 @@ class UserManager(BaseUserManager):
 
     def create_superuser(self, user_id, username, password=None):
         user = self.create_user(user_id=user_id, username=username, password=password)
-        user.is_admin = True
+        user.is_staff = True  
+        user.is_superuser = True  
+        user.is_active = True  
         user.save(using=self._db)
         return user
 
-class User(AbstractBaseUser):
+
+class User(AbstractBaseUser, PermissionsMixin):
     user_id = models.CharField(max_length=50, unique=True)  # For login ID
-    username = models.CharField(max_length=50)               # For display name
+    username = models.CharField(max_length=50)              # For display name
     password = models.CharField(max_length=100)
-    
+
+    is_active = models.BooleanField(default=True)  # 로그인 허용 여부
+    is_staff = models.BooleanField(default=False)  # 관리자 페이지 접근 여부
+
     objects = UserManager()
 
-    USERNAME_FIELD = 'user_id'  # Specify user_id as the field for login
-    REQUIRED_FIELDS = ['username']  # Require username when creating a user
+    USERNAME_FIELD = 'user_id'  # 로그인 시 사용할 필드
+    REQUIRED_FIELDS = ['username']  # createsuperuser 시 입력할 필드
 
     def __str__(self):
         return self.username
-      
 #설문조사 저장 내용
 class UserProfile(models.Model):
     user_id = models.OneToOneField('users.User', on_delete=models.CASCADE)  # Django의 기본 User 모델과 연결
@@ -58,3 +63,11 @@ class Allergy(models.Model):
 
     def __str__(self):
         return f"{self.user.user_id.username} - {self.allergy_name}"
+      
+class UserSurveyRecord(models.Model):
+  user = models.OneToOneField(User, on_delete = models.CASCADE)
+  completed = models.BooleanField(default=False)
+  completed_at = models.DateTimeField(auto_now_add=True)
+  
+  def  __str__(self):
+    return f"{self.user.username} - {'Completed' if self.completed else 'Not Completed'}"
