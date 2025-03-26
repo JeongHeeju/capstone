04f1c2be0f9462ed0815fe92d29b19055396cc9d
JeongHeeju <134275949+JeongHeeju@users.noreply.github.com>
2025-03-26 13:23:15 +0900

    Update serializers.py

--- users/serializers.py
+++ users/serializers.py
@@ -20,41 +20,45 @@ class FoodPreferenceSerializer(serializers.ModelSerializer):
         model = FoodPreference
         fields = ['food_name', 'is_liked']
         extra_kwargs = {
-            'user': {'read_only': True}  # 클라이언트에서 직접 설정하지 못하도록 read_only 설정
+            'user': {'read_only': True}  # 클라이언트에서 직접 설정하지 못하도록
         }
-
 
 class AllergySerializer(serializers.ModelSerializer):
     class Meta:
         model = Allergy
         fields = ['allergy_name']
         extra_kwargs = {
-            'user': {'read_only': True}  # 클라이언트에서 직접 설정하지 못하도록 read_only 설정
+            'user': {'read_only': True}
         }
-    
-#설문조사 저장 부분
+
 class UserProfileSerializer(serializers.ModelSerializer):
     food_preferences = FoodPreferenceSerializer(many=True, required=False)
     allergies = AllergySerializer(many=True, required=False)
 
     class Meta:
         model = UserProfile
-        fields = ['user_id', 'gender', 'birth_date', 'height', 'weight', 'medical_conditions','food_preferences', 'allergies']
-        def create(self, validated_data):
-          food_prefs_data =  validated_data.pop('food_preferences', [])
-          allergy_data = validated_data.pop('allergies', [])
-          user_profile = UserProfile.objects.create(**validated_data)
-          for fp in food_prefs_data:
-            FoodPreference.objects.create(user=user_profile, **fp_data)
-          for al in allergy_data:
-            Allergy.objects.create(user=user_profile, **al_data)
-            
-            return user_profile
-        def update(self, instance, validated_data):
-          return super().update(instance, validated_data)
+        fields = [
+            'user_id', 'gender', 'birth_date', 'height', 'weight', 
+            'medical_conditions', 'food_preferences', 'allergies'
+        ]
         extra_kwargs = {
-            'user_id': {'read_only': True}  # 클라이언트에서 직접 설정하지 못하도록 read_only 설정
+            'user_id': {'read_only': True}
         }
+
+    def create(self, validated_data):
+        food_prefs_data = validated_data.pop('food_preferences', [])
+        allergy_data = validated_data.pop('allergies', [])
+        user_profile = UserProfile.objects.create(**validated_data)
+
+        for fp in food_prefs_data:
+            FoodPreference.objects.create(user=user_profile, **fp)
+        for al in allergy_data:
+            Allergy.objects.create(user=user_profile, **al)
+
+        return user_profile
+
+    def update(self, instance, validated_data):
+        return super().update(instance, validated_data)
 
 
 
