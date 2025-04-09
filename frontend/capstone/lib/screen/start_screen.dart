34f4e5be30a1ffcf8260630121403a4235bb5ea9
JeongHeeju <134275949+JeongHeeju@users.noreply.github.com>
2025-04-09 14:45:34 +0900

    Update start_screen.dart

--- frontend/capstone/lib/screen/start_screen.dart
+++ frontend/capstone/lib/screen/start_screen.dart
@@ -12,41 +12,69 @@ class _StartScreenState extends State<StartScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        child: Stack(
+          clipBehavior: Clip.none,
           children: [
-            Text(
-              '새로운 밥친구를\n사귀어 봐요!',
-              style: TextStyle(fontSize: 30)
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(onPressed: () {
-              Navigator.pushNamed(context, '/login');
-            },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Color(0xFFFF5833),
-                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
-                ),
-                child: Text(
-                  '로그인',
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold,
-                    color: Color(0xFFFBFBFB),
+            // 텍스트 및 버튼
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(height: 40), // 아이콘을 위한 공간 확보
+                Text(
+                  '새로운 밥 친구를\n사귀어 봐요!',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFF2F2F2F),
                   ),
                 ),
-              ),
-            SizedBox(height: 20),
-            TextButton(onPressed: () {
-              Navigator.pushNamed(context, '/signup');
-            }, child: Text('처음 오신 친구인가요? 회원가입',
-                style: TextStyle(color: Color(0xFFFF5833),
+                SizedBox(height: 30),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/login');
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFFFF5833),
+                    padding: EdgeInsets.symmetric(horizontal: 70, vertical: 20),
+                  ),
+                  child: Text(
+                    '로그인',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFFFBFBFB),
+                    ),
+                  ),
                 ),
-              )
+                SizedBox(height: 16),
+                TextButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/signup');
+                  },
+                  child: Text(
+                    '처음 오신 친구인가요? 회원가입',
+                    style: TextStyle(color: Color(0xFFFF5833)),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/chat');
+                  },
+                  child: Text(
+                    '다음에 할게요',
+                    style: TextStyle(color: Color(0xFF9F9F9F)),
+                  ),
+                ),
+              ],
             ),
-            TextButton(onPressed: () {
-              //Navigator.pushNamed(context, routeName) //다음으로 어디화면 갈지
-            }, child: Text('아니요 다음에 할게요',
-                style: TextStyle(color: Color(0xFFFF5833),
-                ),
+
+            Positioned(
+              top: 210,
+              left: -10,
+              child: Image.asset(
+                'assets/icon1.png',
+                height: 60,
               ),
             ),
           ],
