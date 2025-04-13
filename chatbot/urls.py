f2924d5e37dc0da62faab2042eaeb1cf8cc8d667
JeongHeeju <134275949+JeongHeeju@users.noreply.github.com>
2025-04-13 14:25:54 +0900

    Update urls.py

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ChatAPIView
+from .views import ChatAPIView, get_chat_history 
 
 urlpatterns = [
-    path('chat/', ChatAPIView.as_view(), name='chatbot'),  # '/api/chat/'로 접근 가능
+    path('chat/', ChatAPIView.as_view(), name='chatbot'), 
+    path('chat/history/', get_chat_history),
 ]
