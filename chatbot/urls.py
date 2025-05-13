dfc7ab141acfb56211be3a9ae24cc1f56d8da184
JeongHeeju <134275949+JeongHeeju@users.noreply.github.com>
2025-05-13 12:59:24 +0900

    Update urls.py

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import ChatAPIView, get_chat_history 
+from .views import ChatAPIView, proxy_kakao_map_image
 
 urlpatterns = [
     path('chat/', ChatAPIView.as_view(), name='chatbot'), 
     path('chat/history/', get_chat_history),
+    path('proxy/map/', proxy_kakao_map_image, name='proxy_kakao_map'),
 ]
